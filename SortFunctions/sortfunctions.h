1eb4053fbd05f8ae6fb63456eced2e57733f1b82
checkmode <shfwin6@gmail.com>
2020-07-28 15:31:07 +0800

    add windows export

--- SortFunctions/sortfunctions.h
+++ SortFunctions/sortfunctions.h
@@ -1,3 +1,17 @@
 
+#ifndef _SORTFUNCTIONS_H
+#define _SORTFUNCTIONS_H
 
-void sort_bubble(int data[], int len);
+#if defined(_WIN32)
+#if defined(EXPORT_FUN)
+#define DECLSPEC __declspec(dllexport)
+#else
+#define DECLSPEC __declspec(dllimport)
+#endif
+#else
+#define DECLSPEC
+#endif
+
+void DECLSPEC sort_bubble(int data[], int len);
+
+#endif // _SORTFUNCTIONS_H
